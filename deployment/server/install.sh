ed67de89b9bfd9345840c908c84e8c8226b23141
flxdot <felix.fanghanel@nitrex.com>
2024-04-03 20:06:31 +0200

    try stuff

--- deployment/server/install.sh
+++ deployment/server/install.sh
@@ -7,9 +7,9 @@ echo DOMAIN=${DOMAIN}
 echo EMAIL=${EMAIL}
 
 # Phase 1
-docker-compose -f /carlos/deployment/server/docker-compose-initiate.yml --env /carlos/.env up -d nginx
-docker-compose -f /carlos/deployment/server/docker-compose-initiate.yml --env /carlos/.env up certbot
-docker-compose -f /carlos/deployment/server/docker-compose-initiate.yml --env /carlos/.env down
+docker-compose -f /carlos/deployment/server/docker-compose-initiate.yml --env-file /carlos/.env up -d nginx
+docker-compose -f /carlos/deployment/server/docker-compose-initiate.yml --env-file /carlos/.env up certbot
+docker-compose -f /carlos/deployment/server/docker-compose-initiate.yml --env-file /carlos/.env down
 
 # some configurations for let's encrypt
 curl -L --create-dirs -o /carlos/.carlos_data/letsencrypt/options-ssl-nginx.conf https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf
@@ -17,4 +17,4 @@ openssl dhparam -out /carlos/.carlos_data/letsencrypt/ssl-dhparams.pem 2048
 
 # Phase 2
 crontab /carlos/deployment/server/crontab
-docker-compose -f /carlos/deployment/server/docker-compose.yml --env /carlos/.env -d up
+docker-compose -f /carlos/deployment/server/docker-compose.yml --env-file /carlos/.env -d up
