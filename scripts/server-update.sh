e9adea65f51457b14561765d784866da4acfe661
flxdot <felix.fanghanel@nitrex.com>
2024-04-06 10:39:27 +0200

    fix: Deployment script did not update git

--- scripts/server-update.sh
+++ scripts/server-update.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-# This script is used to update the deployed server
+# This script will update the code to the latest available version and restart it
+
+cd /carlos && git pull
+
 docker-compose -f /carlos/deployment/server/docker-compose.yml --env-file /carlos/.env down
 docker-compose -f /carlos/deployment/server/docker-compose.yml --env-file /carlos/.env up --build --detach
