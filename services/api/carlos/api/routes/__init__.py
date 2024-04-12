d28f10b852ee7813b6122c718dbe385f83dcc0cc
flxdot <felix.fanghanel@nitrex.com>
2024-04-12 18:37:27 +0200

    fix: Activate authentication

--- services/api/carlos/api/routes/__init__.py
+++ services/api/carlos/api/routes/__init__.py
@@ -8,7 +8,7 @@ from .device_server_routes import device_server_router
 from .devices_routes import devices_router
 from .health_routes import health_router
 
-main_router = APIRouter(dependencies=[Security(cached_token_verify_from_env)])
+main_router = APIRouter(dependencies=[Security(cached_token_verify_from_env())])
 """This is the main router for the API. It is for routes that require authentication."""
 main_router.include_router(devices_router, prefix="/devices", tags=["devices"])
 
