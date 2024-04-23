0c87e91792aac6da6b68505f5c22fd3a2bb8fb2b
flxdot <felix.fanghanel@nitrex.com>
2024-04-23 22:50:05 +0200

    boost coverage

--- lib/py_edge_interface/carlos/edge/interface/data_directory.py
+++ lib/py_edge_interface/carlos/edge/interface/data_directory.py
@@ -6,7 +6,7 @@ from pathlib import Path
 def find_data_directory(start_path=Path(__file__).parent):
     """Find the data directory for the package."""
 
-    if start_path / ".carlos_data" / ".gitkeep":
+    if (start_path / ".carlos_data" / ".gitkeep").exists():
         return start_path / ".carlos_data"
 
     if start_path == start_path.parent:  # pragma: no cover
